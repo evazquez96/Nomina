4166287dccbc23571ebac86effacae30af60dd93
evazquez96 <evazquez@logsys.com.mx>
2018-09-01 13:59:21 -0500

    Cambios

--- Nomina/Helpers/ValidadorDeContenidoDeCeldasParaEmitir.js
+++ Nomina/Helpers/ValidadorDeContenidoDeCeldasParaEmitir.js
@@ -8,7 +8,7 @@
     /**El primer foreach lo que hace es ponerle la bandera isValid=true**/
     collection.forEach(function (object) {
         //object.isValid = true;
-        console.log(object)
+        console.log(crearPercepcionList(object))
     })
 
 }
@@ -16,6 +16,29 @@
 function getTotalOtrosPagos(Object) {
     var total = 0.0;
 }
+
+function crearPercepcion(TipoPercepcion,Clave,Concepto,ImporteGravado,ImporteExento) {
+    return {
+        TipoPercepcion: TipoPercepcion,
+        Clave: Clave,
+        Concepto: Concepto,
+        ImporteGravado: ImporteGravado,
+        ImporteExento: ImporteExento
+    }
+}
+function crearPercepcionList(object) {
+    var PercepcionList = [];
+    PercepcionList.push(crearPercepcion("001", "001", "Sueldo", object.Sueldo_Gravado,object.Sueldo_Exento));
+    PercepcionList.push(crearPercepcion("002", "002", "Aguinaldo", object.Aguinaldo_Exento, object.Aguinaldo_Exento));
+    var nomina = {
+        PercecpcionList: PercepcionList
+    }
+    return nomina;
+
+}
+function crearDeduccionList(Object) {
+}
+
 function crearObjeto(object) {
     /**
      * Por cada trabajador registrado en nomina se tendra un 
